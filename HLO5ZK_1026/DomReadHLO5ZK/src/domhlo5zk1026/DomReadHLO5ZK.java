58ccc5e3de37127c2d00594ea92f003e782f0631
husoczkidani <71039398+husoczkidani@users.noreply.github.com>
2021-11-09 14:35:52 +0100

    xml gyak 10. 26.

---
+++ HLO5ZK_1026/DomReadHLO5ZK/src/domhlo5zk1026/DomReadHLO5ZK.java
@@ -0,0 +1,46 @@
+package domhlo5zk1026;
+
+import java.io.File;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.SAXException;
+
+import org.w3c.dom.*;
+
+public class DomReadHLO5ZK {
+	
+	public static void main(String[] args) {
+
+		try {
+			File input = new File("usersHLO5ZK.xml");
+			DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+			Document doc = docBuilder.parse(input);
+			
+			doc.getDocumentElement().normalize();
+			
+			System.out.println("Root element: "+ doc.getDocumentElement().getNodeName());
+			
+			NodeList nodes = doc.getElementsByTagName("user");
+			
+			for(int i = 0; i < nodes.getLength();i++) {
+				Node node = nodes.item(i);
+				System.out.println("\nCurrent element: " +node.getNodeName());
+				
+				if(node.getNodeType() == Node.ELEMENT_NODE) {
+					Element element = (Element) node;
+					
+					System.out.println("User id: " + element.getAttribute("id"));
+					System.out.println("First Name: " + element.getElementsByTagName("firstname").item(0).getTextContent());
+					System.out.println("Last Name: " + element.getElementsByTagName("lastname").item(0).getTextContent());
+					System.out.println("Profession: " + element.getElementsByTagName("profession").item(0).getTextContent());
+					
+				}
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ HLO5ZK_1026/DomReadHLO5ZK/src/domhlo5zk1026/DomWriteHLO5ZK.java
@@ -0,0 +1,75 @@
+package domhlo5zk1026;
+
+import java.io.File;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+
+public class DomWriteHLO5ZK {
+
+	public static void main(String[] args) {
+		
+		try {
+			
+			DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+			Document doc = docBuilder.newDocument();
+			
+			Element users = doc.createElementNS("domhlo5zk","users");
+			doc.appendChild(users);
+			
+			users.appendChild(createUser(doc, "01", "Husóczki","Dániel","Programozó"));
+			users.appendChild(createUser(doc, "02", "Mézga", "Géza","Urutazó"));
+			users.appendChild(createUser(doc, "03", "Ero" , "Jeno" , " Sportoló"));
+			
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transf = transformerFactory.newTransformer();
+			
+			transf.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+			transf.setOutputProperty(OutputKeys.INDENT, "yes");
+			transf.setOutputProperty("{https://xml.apache.org/xslt}indent-amount", "2");
+			
+			DOMSource source = new DOMSource(doc);
+			
+			File myFile = new File("users1HLO5ZK.xml");
+			
+			StreamResult console = new StreamResult(System.out);
+			StreamResult file = new StreamResult(myFile);
+			
+			transf.transform(source, console);
+			transf.transform(source, file);
+		}catch(Exception e ) {
+			System.out.println("hiba");
+		}
+
+	}
+	private static Node createUser(Document doc, String id, String firstname, String lastname, String profession) {
+		Element user = doc.createElement("user");
+		
+		user.setAttribute("id", id);
+		user.appendChild(createUserElement(doc,"firstname",firstname));
+		user.appendChild(createUserElement(doc,"lastname",lastname));
+		user.appendChild(createUserElement(doc,"profession",profession));
+		
+		return user;
+	}
+	
+	private static Node createUserElement(Document doc, String name, String value) {
+		Element node = doc.createElement(name);
+		node.appendChild(doc.createTextNode(value));
+		
+		return node;
+	}
+
+}
