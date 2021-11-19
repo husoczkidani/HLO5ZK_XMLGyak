c199029ac427d762799fc4ad0eb5066c42e8d27f
Husóczki Dániel <husoczkidaniel@gmail.com>
2021-11-19 21:21:12 +0100

    Minor changes

--- XMLTaskHLO5ZK/src/hu/domparse/hlo5zk/DOMModifyHLO5ZK.java
+++ XMLTaskHLO5ZK/src/hu/domparse/hlo5zk/DOMModifyHLO5ZK.java
@@ -14,7 +14,6 @@ import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
@@ -108,7 +107,7 @@ public class DOMModifyHLO5ZK {
 		} 
 		// Hibakezeles
 		catch (ParserConfigurationException | SAXException | IOException | TransformerException  e) {
-			System.out.println("Some error occured\nDescription:\n" + e.getMessage());
+			System.out.println("An error occured!\nError Message:\n" + e.getMessage());
 			e.printStackTrace();
 		}
 	}
